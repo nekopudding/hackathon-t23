3ee56033314e5ae18960b3f205c47b6fb3a8c26d
alvinshon <ssw7567@gmail.com>
2022-03-10 15:04:31 -0800

    employee

--- backend/src/model/pathfinder.java
+++ backend/src/model/pathfinder.java
@@ -1,10 +1,6 @@
 package model;
 
 public class pathfinder {
-    private int user_x;
-    private int user_y;
-    private int display_x;
-    private int display_y;
 
     private user user;
     private Item Item;
@@ -14,16 +10,10 @@ public class pathfinder {
         Item = i;
     }
 
-    public pathfinder(int x, int y, int X, int Y) {
-        user_x = x;
-        user_y = y;
-        display_x = X;
-        display_y = Y;
 
-    }
 
     public boolean location(){
-        if (user_x > Item.getDisplay().getX() - 5 || user_y > Item.getDisplay().getY() - 5 || user_x < Item.getDisplay().getX() +5 || user_y < Item.getDisplay().getY() + 5)
+        if (user.getX() > Item.getDisplay().getX() - 5 || user.getY() > Item.getDisplay().getY() - 5 || user.getX() < Item.getDisplay().getX() +5 || user.getY() < Item.getDisplay().getY() + 5)
             return true;
         else
             return false;
