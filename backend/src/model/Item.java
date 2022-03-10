bcbc78a542cbc7cd4388952475349b00be569865
aeshaq <abdallahishaq1010#gmail.com>
2022-03-10 13:16:19 -0800

    ## item

--- backend/src/model/Item.java
+++ backend/src/model/Item.java
@@ -1,4 +1,32 @@
 package model;
 
 public class Item {
+    private String name;
+    private int stock;
+    private int x;
+    private int y;
+
+    public Item(String n, int st, int x, int y) {
+        name = n;
+        stock = st;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
 }
