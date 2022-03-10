7ba779a6b008e4fe2696df7ddcd04876c4407ebb
alvinshon <ssw7567@gmail.com>
2022-03-10 13:18:44 -0800

    employee

---
+++ backend/src/model/employee.java
@@ -0,0 +1,17 @@
+package model;
+
+public class employee {
+    private int x;
+    private int y;
+    private int ID;
+    private String name;
+
+    public employee(int x, int y, int ID, String name) {
+        this.x = x;
+        this.y = y;
+        this.ID = ID;
+        this.name = name;
+    }
+    
+
+}
